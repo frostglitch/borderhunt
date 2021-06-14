76d5e445e94fa0e6160137903d4275d979b20bb0
frostglitch <dr.dulitl.sim@gmail.com>
2021-06-14 18:23:28 +0200

    Initial eclipse project

---
+++ src/me/Allt/borderhunt/Main.java
@@ -0,0 +1,18 @@
+package me.Allt.borderhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+	  @Override
+	  public void onEnable()
+	  {
+		    
+	  }
+	  
+	  @Override
+	  public void onDisable()
+	  {
+		    
+	  }
+}
